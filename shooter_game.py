3560bfdfdf298d7e835b93512726518a7800e24f
Назар Юрчик <145269462+YrcNazar@users.noreply.github.com>
2024-04-07 15:14:49 +0300

    Update shooter_game.py
    Додали кулі, постріли іііі все.......

--- shooter_game.py
+++ shooter_game.py
@@ -10,15 +10,23 @@ fire_sound = mixer.Sound('fire.ogg')
 
 font.init()
 font1 = font.Font(None, 36)
+font2 = font.Font(None, 80)
+win = font2.render("You Win", True, (255, 255, 255))
+lose = font2.render("You Lose", True, (180, 0, 0))
+
 
 
 # нам потрібні такі картинки:
-img_back = "galaxy.jpg"  # фон гри
-img_hero = "rocket.png"  # герой
+img_back = "galaxy.jpg"  
+img_hero = "rocket.png" 
 img_enemy = 'ufo.png'
+img_bullet = 'bullet.png'
 
 score = 0
 lost = 0
+max_lost = 3
+max_win = 30
+
 
 # клас-батько для інших спрайтів
 class GameSprite(sprite.Sprite):
@@ -31,21 +39,17 @@ class GameSprite(sprite.Sprite):
             image.load(player_image), (size_x, size_y))
         self.speed = player_speed
 
-
         # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
 
-
     # метод, що малює героя на вікні
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-
 # клас головного гравця
 class Player(GameSprite):
-
 
     # метод для керування спрайтом стрілками клавіатури
     def update(self):
@@ -55,10 +59,12 @@ class Player(GameSprite):
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
 
-
     # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+
 
 class Enemy(GameSprite):
     def update(self):
@@ -68,6 +74,13 @@ class Enemy(GameSprite):
             self.rect.x = randint(80, win_width - 80)
             self.rect.y = 0
             lost = lost + 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+        
 # створюємо віконце
 win_width = 700
 win_height = 500
@@ -75,27 +88,31 @@ display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
 
-
 # створюємо спрайти
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 monsters = sprite.Group()
 for i in range(1, 7):
     monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1,5))
     monsters.add(monster)
+
+bullets = sprite.Group()
+ 
+
 # змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
 finish = False
 
-
 # Основний цикл гри:
 run = True  # прапорець скидається кнопкою закриття вікна
-
 
 while run:
     # подія натискання на кнопку Закрити
     for e in event.get():
         if e.type == QUIT:
             run = False
-
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
 
     if not finish:
         # оновлюємо фон
@@ -108,11 +125,12 @@ while run:
         # рухи спрайтів
         ship.update()
         monsters.update()
+        bullets.update()
 
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
         monsters.draw(window)
-
+        bullets.draw(window)
         display.update()
     # цикл спрацьовує кожні 0.05 секунд
     time.delay(40)
